f2e1952d795e9367b890c43e0c0386a5b50e1816
DarioSantosSousa <Dariovsgeovanna@gmail.com>
2022-04-18 00:10:56 -0300

    Adição da autenticação

--- Controllers/LivroController.cs
+++ Controllers/LivroController.cs
@@ -1,5 +1,7 @@
+using System;
 using Biblioteca.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace Biblioteca.Controllers
 {
@@ -7,6 +9,10 @@ namespace Biblioteca.Controllers
     {
         public IActionResult Cadastro()
         {
+            //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            Autenticacao.CheckLogin(this);//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            Autenticacao.verificaSeUsuarioeAdmin(this);//XXXXXXXXXXXXXXXXXXXXXXX
+
             Autenticacao.CheckLogin(this);
             return View();
         }
@@ -14,9 +20,13 @@ namespace Biblioteca.Controllers
         [HttpPost]
         public IActionResult Cadastro(Livro l)
         {
+            //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            Autenticacao.CheckLogin(this);//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            Autenticacao.verificaSeUsuarioeAdmin(this);//XXXXXXXXXXXXXXXXXXXXXXX
+
             LivroService livroService = new LivroService();
 
-            if(l.Id == 0)
+            if (l.Id == 0)
             {
                 livroService.Inserir(l);
             }
@@ -28,26 +38,40 @@ namespace Biblioteca.Controllers
             return RedirectToAction("Listagem");
         }
 
-        public IActionResult Listagem(string tipoFiltro, string filtro)
+        public IActionResult Listagem(string tipoFiltro, string filtro, string itensPorPagina, int NumDaPagina, int PaginaAtual)
         {
             Autenticacao.CheckLogin(this);
             FiltrosLivros objFiltro = null;
-            if(!string.IsNullOrEmpty(filtro))
+
+            if (!string.IsNullOrEmpty(filtro))
             {
                 objFiltro = new FiltrosLivros();
                 objFiltro.Filtro = filtro;
                 objFiltro.TipoFiltro = tipoFiltro;
             }
+            ViewData["livrosPorPagina"] = (string.IsNullOrEmpty(itensPorPagina) ? 10 : Int32.Parse(itensPorPagina));
+            ViewData["PaginaAtual"] = (PaginaAtual != 0 ? PaginaAtual : 1);
+
             LivroService livroService = new LivroService();
             return View(livroService.ListarTodos(objFiltro));
         }
 
         public IActionResult Edicao(int id)
         {
-            Autenticacao.CheckLogin(this);
+            //XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            Autenticacao.CheckLogin(this);//XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+            Autenticacao.verificaSeUsuarioeAdmin(this);//XXXXXXXXXXXXXXXXXXXXXXX
+
             LivroService ls = new LivroService();
             Livro l = ls.ObterPorId(id);
             return View(l);
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
